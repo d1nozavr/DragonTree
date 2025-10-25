c4db36298467fdb465f5d5afb46f65b25650d311
Yaroslav <trifonovyaroslav@internet.ru>
2025-10-25 09:00:26 +0300

    Add output to 'Parser'

--- README.md
+++ README.md
@@ -4,6 +4,26 @@ DragonTree
 > [!WARNING]
 > This project is still in development!
 
+Author: d1nozavr (Trifonov Yaroslav)
+---
+
+Examples
+---
+### Version **0.0.1 (alpha)**
+Code:
+```
+output: 2+2
+output: -3
+output: 44
+```
+
+Result:
+```
+4
+-3
+44
+```
+
 Versions
 ---
 Current *available* version: **0.0.1 (alpha)**
@@ -12,7 +32,7 @@ Installation
 ---
 
 ### Dependencies
-> You need to have only "**Python**".
+> You need to have only "**Python**" and "**git**".
 
 > **Python** version is **3.14** *recommended*.
 
@@ -23,7 +43,7 @@ cd DragonTree
 python interpreter.py
 ```
 
-### Linux
+### Linux/MacOS
 ```
 git clone https://github.com/d1nozavr/DragonTree.git
 cd DragonTree
--- core/Parser.py
+++ core/Parser.py
@@ -6,6 +6,7 @@ Parser
 from core.Ast import (
     BinOpNode,
     NumberNode,
+    OutputNode,
 )
 from core.TokenType import TokenType
 
@@ -24,9 +25,24 @@ class Parser:
         self.pos += 1
 
     def parse(self):
-        results = self.expr()
-        
-        return results
+        result = self.statement()
+        return result
+
+    def statement(self):
+        token = self.peek()
+
+        if token.type == TokenType.OUTPUT:
+            self.advance()
+
+            token = self.peek()
+            if token.type == TokenType.COLON:
+                self.advance()
+                right = self.expr()
+                return OutputNode(right)
+
+            raise SyntaxError("Need ':' after 'output'")
+
+        raise SyntaxError("Invalid syntax")
 
     def expr(self):
         left = self.term()
@@ -56,13 +72,13 @@ class Parser:
             if token.type in (TokenType.MULTIPLY, TokenType.DIVIDE):
                 operation = token
                 self.advance()
-                
+
                 right = self.factor()
 
                 left = BinOpNode(left, operation.value, right).evaluate()
 
                 continue
-            
+
             break
         return left
 
@@ -72,17 +88,17 @@ class Parser:
         if token.type == TokenType.NUMBER:
             self.advance()
             return NumberNode(token.value).evaluate()
-        
+
         elif token.type == TokenType.PLUS:
             self.advance()
 
             right = self.factor()
             return BinOpNode(None, token.value, right).evaluate()
-        
+
         elif token.type == TokenType.MINUS:
             self.advance()
 
             right = self.factor()
             return BinOpNode(None, token.value, right).evaluate()
-        
+
         raise RuntimeError(f"Unexpected token '{token}' at pos {self.pos}")
