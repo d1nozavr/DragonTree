b8d17f7a576ffb75f093f3043f15f2ec7fd43a6c
Yaroslav <trifonovyaroslav@internet.ru>
2025-10-25 22:34:57 +0300

    Add string, equals to 'TokenType'

--- core/TokenType.py
+++ core/TokenType.py
@@ -8,12 +8,16 @@ from enum import Enum
 
 class TokenType(Enum):
     NUMBER = "NUMBER"
+    STRING = "STRING"
+
     ID = "ID"
+    EQUALS = "EQUALS"
 
     PLUS = "PLUS"
     MINUS = "MINUS"
     MULTIPLY = "MULTIPLY"
     DIVIDE = "DIVIDE"
+
     COLON = "COLON"
 
     OUTPUT = "OUTPUT"
